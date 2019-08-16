c4254cb533c7b29f7ae3eb61454afd4fc156bf05
Felipe Romero <feliperomero3@outlook.com>
2019-08-16 16:54:53 -0500

    Add ProductController.

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ViewResult List() => View(_repository.Products);
+    }
+}
