8e365b157a8c52cdba9d509bcc2d1f432784f4a3
Felipe Romero <feliperomero3@outlook.com>
2019-08-16 17:38:25 -0500

    Add ApplicationDbContext.

---
+++ SportsStore/Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
+namespace SportsStore.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
