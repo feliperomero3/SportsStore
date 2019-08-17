952d21fc20b430cd546f891bd08174dfdd308d99
Felipe Romero <feliperomero3@outlook.com>
2019-08-16 22:49:53 -0500

    Initialize the database with test data.

---
+++ SportsStore/Context/DbInitializer.cs
@@ -0,0 +1,87 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
+namespace SportsStore.Context
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(ApplicationDbContext context)
+        {
+            context.Database.Migrate();
+
+            // Look for any products.
+            if (context.Products.Any())
+            {
+                return; // Db has been seeded
+            }
+
+            context.Products.AddRange(
+                new Product
+                {
+                    Name = "Kayak",
+                    Description = "A boat for one person",
+                    Category = "Water sports",
+                    Price = 275
+                },
+                new Product
+                {
+                    Name = "Life jacket",
+                    Description = "Protective and fashionable",
+                    Category = "Waters sports",
+                    Price = 48.95
+                },
+                new Product
+                {
+                    Name = "Soccer Ball",
+                    Description = "FIFA-approved size and weight",
+                    Category = "Soccer",
+                    Price = 19.50
+                },
+                new Product
+                {
+                    Name = "Corner Flags",
+                    Description = "Give your playing field a professional touch",
+                    Category = "Soccer",
+                    Price = 34.95
+                },
+                new Product
+                {
+                    Name = "Stadium",
+                    Description = "Flat-packed 35,000-seat stadium",
+                    Category = "Soccer",
+                    Price = 79500
+                },
+                new Product
+                {
+                    Name = "Thinking Cap",
+                    Description = "Improve brain efficiency by 75%",
+                    Category = "Chess",
+                    Price = 16
+                },
+                new Product
+                {
+                    Name = "Unsteady Chair",
+                    Description = "Secretly give your opponent a disadvantage",
+                    Category = "Chess",
+                    Price = 29.95
+                },
+                new Product
+                {
+                    Name = "Human Chess Board",
+                    Description = "A fun game for the family",
+                    Category = "Chess",
+                    Price = 75
+                },
+                new Product
+                {
+                    Name = "Bling-Bling King",
+                    Description = "Gold-plated, diamond-studded King",
+                    Category = "Chess",
+                    Price = 1200
+                });
+
+            context.SaveChanges();
+        }
+    }
+}
--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using SportsStore.Context;
 
 namespace SportsStore
 {
@@ -7,7 +11,26 @@ namespace SportsStore
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+
+                    DbInitializer.Initialize(context);
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
