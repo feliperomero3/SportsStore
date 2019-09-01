a3de86c9fcac3300255bb391ff2fbec7ecd430cd
Felipe Romero <feliperomero3@outlook.com>
2019-08-31 20:05:25 -0500

    Build a category navigation menu.

---
+++ SportsStore.Tests/NavigationMenuViewComponentTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Moq;
+using SportsStore.Components;
+using SportsStore.Models;
+using Xunit;
+using Xunit.Sdk;
+
+namespace SportsStore.Tests
+{
+    public class NavigationMenuViewComponentTests
+    {
+        [Fact]
+        public void Can_Select_Categories()
+        {
+            // Arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new[]
+            {
+                new Product { ProductId = 1, Name = "P1", Category = "Apples" },
+                new Product { ProductId = 2, Name = "P2", Category = "Apples" },
+                new Product { ProductId = 3, Name = "P3", Category = "Plums" },
+                new Product { ProductId = 4, Name = "P4", Category = "Oranges" },
+            });
+
+            var target = new NavigationMenuViewComponent(mock.Object);
+
+            // Act
+            var results = target.Invoke() as ViewViewComponentResult;
+
+            if (results == null) throw new XunitException("results is null.");
+
+            var categories = (IEnumerable<string>)results.ViewData.Model;
+
+            // Assert
+            Assert.True(new[] { "Apples", "Oranges", "Plums" }.SequenceEqual(categories));
+        }
+    }
+}
---
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private readonly IProductRepository _repository;
+
+        public NavigationMenuViewComponent(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(_repository.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x));
+        }
+    }
+}
