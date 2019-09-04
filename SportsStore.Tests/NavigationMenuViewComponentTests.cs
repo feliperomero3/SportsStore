3b616bd3f721483f7f7b70ed3bba1d160e186f2d
Felipe Romero <feliperomero3@outlook.com>
2019-09-04 18:56:54 -0500

    Unit test highlight the current category.

--- SportsStore.Tests/NavigationMenuViewComponentTests.cs
+++ SportsStore.Tests/NavigationMenuViewComponentTests.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Routing;
 using Moq;
 using SportsStore.Components;
 using SportsStore.Models;
@@ -35,6 +37,40 @@ namespace SportsStore.Tests
 
             // Assert
             Assert.True(new[] { "Apples", "Oranges", "Plums" }.SequenceEqual(categories));
+        }
+
+        [Fact]
+        public void Indicates_Selected_Category()
+        {
+            // Arrange
+            const string categoryToSelect = "Apples";
+
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new[]
+            {
+                new Product { ProductId = 1, Name = "P1", Category = "Apples" },
+                new Product { ProductId = 4, Name = "P2", Category = "Oranges" },
+            });
+
+            var target = new NavigationMenuViewComponent(mock.Object)
+            {
+                ViewComponentContext = new ViewComponentContext
+                {
+                    ViewContext = new ViewContext { RouteData = new RouteData() }
+                }
+            };
+
+            target.RouteData.Values["category"] = categoryToSelect;
+
+            // Act
+            var results = target.Invoke() as ViewViewComponentResult;
+
+            if (results == null) throw new XunitException("results is null.");
+
+            var selectedCategory = (string)results.ViewData["SelectedCategory"];
+
+            // Assert
+            Assert.Equal(categoryToSelect, selectedCategory);
         }
     }
 }
