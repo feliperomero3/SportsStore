af958c36e4d8afdd07210c8a5dcfe47daab0bb85
Felipe Romero <feliperomero3@outlook.com>
2019-10-09 23:26:35 -0500

    Define the Cart Model.

---
+++ SportsStore/Models/Cart.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SportsStore.Models
+{
+    public class Cart
+    {
+        private List<CartLine> _lineCollection = new List<CartLine>();
+
+        public virtual void AddItem(Product product, int quantity)
+        {
+            var line = _lineCollection
+                .FirstOrDefault(p => p.Product.ProductId == product.ProductId);
+
+            if (line == null)
+            {
+                _lineCollection.Add(new CartLine
+                {
+                    Product = product,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        public virtual void RemoveLine(Product product) =>
+            _lineCollection.RemoveAll(l => l.Product.ProductId == product.ProductId);
+
+        public virtual double ComputeTotalValue() =>
+            _lineCollection.Sum(e => e.Product.Price * e.Quantity);
+
+        public virtual void Clear() => _lineCollection.Clear();
+
+        public virtual IEnumerable<CartLine> Lines => _lineCollection;
+    }
+}
---
+++ SportsStore/Models/CartLine.cs
@@ -0,0 +1,9 @@
+﻿namespace SportsStore.Models
+{
+    public class CartLine
+    {
+        public int CartLineId { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
