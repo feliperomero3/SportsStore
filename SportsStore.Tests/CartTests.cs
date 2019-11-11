c9c267b24d64e7c72004336408f405d5ec0d2615
Felipe Romero <feliperomero3@outlook.com>
2019-11-10 21:03:52 -0600

    Unit test the Cart Model.

---
+++ SportsStore.Tests/CartTests.cs
@@ -0,0 +1,122 @@
+﻿using System.Linq;
+using SportsStore.Models;
+using Xunit;
+
+namespace SportsStore.Tests
+{
+    public class CartTests
+    {
+        [Fact]
+        public void Can_Add_New_Lines()
+        {
+            // Arrange - create some test products
+            var p1 = new Product { ProductId = 1, Name = "P1" };
+            var p2 = new Product { ProductId = 2, Name = "P2" };
+
+            // Arrange - create a new cart
+            var target = new Cart();
+
+            // Act
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+
+            var results = target.Lines.ToArray();
+
+            // Assert
+            Assert.Equal(2, results.Length);
+            Assert.Equal(p1, results[0].Product);
+            Assert.Equal(p2, results[1].Product);
+        }
+
+        [Fact]
+        public void Can_Add_Quantity_For_Existing_Lines()
+        {
+            // Arrange - create some test products
+            var p1 = new Product { ProductId = 1, Name = "P1" };
+            var p2 = new Product { ProductId = 2, Name = "P2" };
+
+            // Arrange - create a new cart
+            var target = new Cart();
+
+            // Act
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            target.AddItem(p1, 10);
+
+            var results = target.Lines
+                .OrderBy(c => c.Product.ProductId).ToArray();
+
+            // Assert
+            Assert.Equal(2, results.Length);
+            Assert.Equal(11, results[0].Quantity);
+            Assert.Equal(1, results[1].Quantity);
+        }
+
+        [Fact]
+        public void Can_Remove_Line()
+        {
+            // Arrange - create some test products
+            var p1 = new Product { ProductId = 1, Name = "P1" };
+            var p2 = new Product { ProductId = 2, Name = "P2" };
+            var p3 = new Product { ProductId = 3, Name = "P3" };
+
+            // Arrange - create a new cart
+            var target = new Cart();
+
+            // Arrange - add some products to the cart
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 3);
+            target.AddItem(p3, 5);
+            target.AddItem(p2, 1);
+
+            // Act
+            target.RemoveLine(p2);
+
+            // Assert
+            Assert.Equal(0, target.Lines.Count(c => c.Product == p2));
+            Assert.Equal(2, target.Lines.Count());
+        }
+
+        [Fact]
+        public void Calculate_Cart_Total()
+        {
+            // Arrange - create some test products
+            var p1 = new Product { ProductId = 1, Name = "P1", Price = 100D };
+            var p2 = new Product { ProductId = 2, Name = "P2", Price = 50D };
+
+            // Arrange - create a new cart
+            var target = new Cart();
+
+            // Act
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            target.AddItem(p1, 3);
+
+            var result = target.ComputeTotalValue();
+
+            // Assert
+            Assert.Equal(450D, result);
+        }
+
+        [Fact]
+        public void Can_Clear_Contents()
+        {
+            // Arrange - create some test products
+            var p1 = new Product { ProductId = 1, Name = "P1", Price = 100D };
+            var p2 = new Product { ProductId = 2, Name = "P2", Price = 50D };
+
+            // Arrange - create a new cart
+            var target = new Cart();
+
+            // Arrange - add some items
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+
+            // Act - reset the cart
+            target.Clear();
+
+            // Assert
+            Assert.Empty(target.Lines);
+        }
+    }
+}
