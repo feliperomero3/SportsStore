d8b795758cff2888b30226c867b55bf22703c62a
Felipe Romero <feliperomero3@outlook.com>
2019-12-06 22:08:02 -0600

    Add the Cart Summary Widget.

---
+++ SportsStore/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private readonly Cart _cart;
+
+        public CartSummaryViewComponent(Cart cart)
+        {
+            _cart = cart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(_cart);
+        }
+    }
+}
