4d25069fe160b6b12d712d553f72983de8ba5475
Felipe Romero <feliperomero3@outlook.com>
2019-12-07 20:38:48 -0600

    Add the Checkout Process.

---
+++ SportsStore/Controllers/OrderController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class OrderController : Controller
+    {
+        public ViewResult Checkout() => View(new Order());
+    }
+}
