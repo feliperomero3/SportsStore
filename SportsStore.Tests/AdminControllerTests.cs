f6c5457436a3a03ae1c6d9a4c74eea3f63e8ac4d
Felipe Romero <feliperomero3@outlook.com>
2019-12-11 20:28:24 -0600

    Add AdminController with unit test.

---
+++ SportsStore.Tests/AdminControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SportsStore.Controllers;
+using SportsStore.Models;
+using Xunit;
+
+namespace SportsStore.Tests
+{
+    public class AdminControllerTests
+    {
+        [Fact]
+        public void Index_Contains_All_Products()
+        {
+            // Arrange - create the mock repository
+            var mock = new Mock<IProductRepository>();
+
+            mock.Setup(m => m.Products).Returns(new[]
+            {
+                new Product { ProductId = 1, Name = "P1" },
+                new Product { ProductId = 2, Name = "P2" },
+                new Product { ProductId = 3, Name = "P3" },
+            }.AsQueryable());
+
+            // Arrange - create a controller
+            var target = new AdminController(mock.Object);
+
+            // Action
+            var result = GetViewModel<IEnumerable<Product>>(target.Index())?.ToArray();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(3, result.Length);
+            Assert.Equal("P1", result[0].Name);
+            Assert.Equal("P2", result[1].Name);
+            Assert.Equal("P3", result[2].Name);
+        }
+
+        private static T GetViewModel<T>(IActionResult result) where T : class
+        {
+            return (result as ViewResult)?.ViewData.Model as T;
+        }
+    }
+}
---
+++ SportsStore/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IProductRepository _repository;
+
+        public AdminController(IProductRepository repo)
+        {
+            _repository = repo;
+        }
+
+        public ViewResult Index() => View(_repository.Products);
+    }
+}
