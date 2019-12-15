851f250d6481797423923275d64d3424c5abab36
Felipe Romero <feliperomero3@outlook.com>
2019-12-14 21:22:45 -0600

    Add Model Validation.

--- SportsStore/Models/Product.cs
+++ SportsStore/Models/Product.cs
@@ -1,15 +1,22 @@
-﻿namespace SportsStore.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportsStore.Models
 {
     public class Product
     {
         public int ProductId { get; set; }
 
+        [Required(ErrorMessage = "Please enter a product name")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Please enter a description")]
         public string Description { get; set; }
 
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
         public double Price { get; set; }
 
+        [Required(ErrorMessage = "Please specify a category")]
         public string Category { get; set; }
     }
 }
