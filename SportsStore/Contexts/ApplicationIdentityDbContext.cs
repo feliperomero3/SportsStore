87f39642bb2dfe5cffce36773bff0df8ad8f4183
Felipe Romero <feliperomero3@outlook.com>
2019-12-17 22:24:38 -0600

    Add the Context Class for the Identity Database.

---
+++ SportsStore/Contexts/ApplicationIdentityDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SportsStore.Contexts
+{
+    public class ApplicationIdentityDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationIdentityDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
